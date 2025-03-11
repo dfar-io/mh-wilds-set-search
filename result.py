427131d427ee3979c367d13e7cc593c455be59e0
Dave Farinelli <d@dfar.io>
2025-03-11 16:21:01 -0400

    Add slots, group/series skills (#4)

--- result.py
+++ result.py
@@ -7,10 +7,12 @@ class Result:
         self.legs = legs
 
     def __repr__(self):
+        combined_slots = self.get_combined_slots()
         return (
             f'{self.get_defense()}: {self.head.name}, {self.chest.name}, {self.arms.name}, {self.waist.name}, {self.legs.name}\n'
             f'{self.get_combined_skills()}\n'
             f'F: {self.head.fire} W: {self.head.water} T: {self.head.thunder} I: {self.head.ice} D: {self.head.dragon}\n'
+            f'lv1: {combined_slots[0]} lv2: {combined_slots[1]} lv3: {combined_slots[2]}\n'
         )
 
     def get_defense(self):
@@ -31,8 +33,13 @@ class Result:
         return aggregated
     
     def matches_search_criteria(self, search_criteria):
-        combined_skills = self.get_combined_skills()
+        # check slots
+        combined_slots = self.get_combined_slots()
+        if combined_slots[0] < search_criteria.min_slots[0] or combined_slots[1] < search_criteria.min_slots[1] < combined_slots[2] < search_criteria.min_slots[2]:
+            return False
 
+        # check skills
+        combined_skills = self.get_combined_skills()
         for skill in search_criteria.skills:
             search_criteria_skill_name = list(skill)[0]
             if search_criteria_skill_name not in combined_skills:
@@ -43,3 +50,10 @@ class Result:
                 return False
 
         return True
+
+    def get_combined_slots(self):
+        return [
+            self.head.get_slot_level_count(1) + self.chest.get_slot_level_count(1) + self.arms.get_slot_level_count(1) + self.waist.get_slot_level_count(1) + self.legs.get_slot_level_count(1),
+            self.head.get_slot_level_count(2) + self.chest.get_slot_level_count(2) + self.arms.get_slot_level_count(2) + self.waist.get_slot_level_count(2) + self.legs.get_slot_level_count(2),
+            self.head.get_slot_level_count(3) + self.chest.get_slot_level_count(3) + self.arms.get_slot_level_count(3) + self.waist.get_slot_level_count(3) + self.legs.get_slot_level_count(3)
+        ]
--- search_criteria.py
+++ search_criteria.py
@@ -1,9 +1,13 @@
 class SearchCriteria:
-    def __init__(self, skills):
+    def __init__(self, skills, min_slots):
         self.skills = skills
+        self.min_slots = min_slots
 
     def __repr__(self):
         return (
             f'criteria:\n'
             f'skills: {self.skills}\n'
+            f'min level 1 slots: {self.min_slots[0]}\n'
+            f'min level 2 slots: {self.min_slots[1]}\n'
+            f'min level 3 slots: {self.min_slots[2]}\n'
         )
